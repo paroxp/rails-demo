ae7d4e53fbc95eefc73eeab764042ecaccd9b615
Rafal Proszowski <rafal.proszowski@digital.cabinet-office.gov.uk>
2016-08-30 12:29:48 +0100

    Add basic authentication

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  http_basic_authenticate_with name: "admin", password: "password", except: [:index, :show]
+
   def index
     @articles = Article.all
   end
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,6 @@
 class CommentsController < ApplicationController
+  http_basic_authenticate_with name: "admin", password: "password", only: :destroy
+
   def create
     @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
